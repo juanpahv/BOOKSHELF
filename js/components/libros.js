1353416eb72ee91ff329141c33ee1cb0dc624757
claudios883 <claudioalejandro4@gmail.com>
2022-11-23 18:00:48 -0600

    qq-

---
+++ js/components/libros.js
@@ -0,0 +1,324 @@
+export const libros = {
+    "Antes de diciembre" :
+    {
+        titulo : "Antes de diciembre",
+        autor : "Joana Marcus",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Yo antes de ti" :
+    {
+        titulo : "Yo antes de ti",
+        autor : "Jojo Moyes",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Eleanor y Park" :
+    {
+        titulo : "Eleanor y Park",
+        autor : "Andre Aciman",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Cuando no queden mas estrellas" :
+    {
+        titulo : "Cuando no queden mas estrellas",
+        autor : "Maria Martinez",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Bajo la misma estrella" :
+    {
+        titulo : "Bajo la misma estrella",
+        autor : "John Green",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Orgullo y prejuicio" :
+    {
+        titulo : "Orgullo y prejuicio",
+        autor : "Ane Austen",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Nosotros en la luna" :
+    {
+        titulo : "Nosotros en la luna",
+        autor : "Alice Kellen",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Llamame por tu nombre" :
+    {
+        titulo : "Llamame por tu nombre",
+        autor : "Jojo Moyes",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Un cuento perfecto" :
+    {
+        titulo : "Un cuento perfecto",
+        autor : "Elisabet Benavent",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Cumbres Borrascosas" :
+    {
+        titulo : "Cumbres Borrascosas",
+        autor : "Emily Bronte",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "El país de un solo hombre" :
+    {
+        titulo : "El país de un solo hombre",
+        autor : "Alfonso Zarate",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "La república o el estado" :
+    {
+        titulo : "La república o el estado",
+        autor : "Platón",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "El contrato social" :
+    {
+        titulo : "El contrato social",
+        autor : "Jean Jacques Rousseau",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Una tierra prometida" :
+    {
+        titulo : "Una tierra prometida",
+        autor : "Barack Obama",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Política" :
+    {
+        titulo : "Política",
+        autor : "Aristóteles",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "No me hables de política" :
+    {
+        titulo : "No me hables de política",
+        autor : "Berta Barbet",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Rebelión en la granja" :
+    {
+        titulo : "Rebelión en la granja",
+        autor : "George Orwell",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Farándula" :
+    {
+        titulo : "Farándula",
+        autor : "Marta Sanz",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "!Indignaos¡" :
+    {
+        titulo : "!Indignaos¡",
+        autor : "Stéphane Hessel",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "El príncipe" :
+    {
+        titulo : "El príncipe",
+        autor : "Maquiavelo",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Ecos del pasado" :
+    {
+        titulo : "Ecos del pasado",
+        autor : "Charlotte Link",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "El ladrón de cuerpos" :
+    {
+        titulo : "El ladrón de cuerpos",
+        autor : "Anne Rice",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "El secuestro de la hermana Tegualda" :
+    {
+        titulo : "El secuestro de la hermana Tegualda",
+        autor : "Hernán Rivera",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Isla en negro" :
+    {
+        titulo : "Isla en negro",
+        autor : "Leopoldo Luis",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "It" :
+    {
+        titulo : "It",
+        autor : "Stephen King",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "La casa del nazi" :
+    {
+        titulo : "La casa del nazi",
+        autor : "Xabier Quiroga",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Las manos de Orlac" :
+    {
+        titulo : "Las manos de Orlac",
+        autor : "Maurice Renard",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Pet sematary" :
+    {
+        titulo : "Pet sematary",
+        autor : "Stephen King",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Sin rastro" :
+    {
+        titulo : "Sin rastro",
+        autor : "Alex Smith",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Tipos que no duermen por la noche" :
+    {
+        titulo : "Tipos que no duermen por la noche",
+        autor : "Iván Farías",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Alimentos seguros microbiologia" :
+    {
+        titulo : "Alimentos seguros microbiologia",
+        autor : "Stephen Forsythe<",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Automatizacion electroneumatica: metodos sistematicos" :
+    {
+        titulo : "Automatizacion electroneumatica: metodos sistematicos",
+        autor : "uan Camilo Vázquez Cortes",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Curso practico para programacion de avr..." :
+    {
+        titulo : "Curso practico para programacion de avr...",
+        autor : "Ernesto Paredes Martínez",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Diagramas binarios. Teoria y aplicaciones" :
+    {
+        titulo : "Diagramas binarios. Teoria y aplicaciones",
+        autor : "",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Drones: modelado y control de cuadrotores" :
+    {
+        titulo : "Drones: modelado y control de cuadrotores",
+        autor : "Roger Miranda Colorado",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "E renovarse o morir" :
+    {
+        titulo : "E renovarse o morir",
+        autor : "Silvia Leal",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Horizontes digitales" :
+    {
+        titulo : "Horizontes digitales",
+        autor : "Diego Lizarazo Arias",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Impresion 3D" :
+    {
+        titulo : "Impresion 3D",
+        autor : "Sergio Gómez González",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "¿Nos enferma la tecnología?" :
+    {
+        titulo : "¿Nos enferma la tecnología?",
+        autor : "Ian Douglas",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    },
+    "Robots sociales: del temor a la esperanza en los sirvientes mecanicos" :
+    {
+        titulo : "Robots sociales: del temor a la esperanza en los sirvientes mecanicos",
+        autor : "omas Perales Benito",
+        editorial : "",
+        ISBN : 0,
+        sinopsis : ""
+    }
+};
+
+// export default libros;
